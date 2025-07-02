20f7012f6a9f26de50d46e2044c9d621f975e65c
E-Parker <ethanliamparker@proton.me>
2025-07-02 17:42:17 -0400

    Replaced List and Hashtable with more up-to-date versions.

--- inc/hash_table.h
+++ inc/hash_table.h
@@ -4,38 +4,40 @@
 extern "C" {
 #endif
 
-#include <stdint.h>
 #include <stdbool.h>
 
-#include "cStringUtilities.h"
-
 #define Pow2Ceiling(T, num) ((T)internal_Pow2Ceiling(sizeof(T), num))
-uint64_t internal_Pow2Ceiling(uint64_t size, uint64_t num);
+
+unsigned long long fnvHash64(const short* buffer, const short* const bufferEnd);
+short* FindBufferEnd(const short* buffer);
+unsigned long long internal_Pow2Ceiling(unsigned long long size, unsigned long long num);
 
 typedef struct {
     /* An item in a hash table. */
-    char* Key;
+    short* Key;
     void* Value;
 } HashTableItem;
 
 typedef struct HashTable {
-    uint64_t Size;
-    uint64_t SlotsUsed;
-    uint64_t* ActiveIndicies;
+    unsigned long long Size;
+    unsigned long long SlotsUsed;
+    unsigned long long* ActiveIndicies;
     HashTableItem* Array;
 } HashTable;
 
-HashTable* internal_HashTable_create(uint64_t itemSize, uint64_t size);
+HashTable* internal_HashTable_create(unsigned long long itemSize, unsigned long long size);
 void HashTable_destroy(HashTable** table);
-char* HashTable_insert(HashTable* table, const char* alias, void* value);
-void HashTable_remove(HashTable* table, const char* alias);
-void HashTable_resize(HashTable* table, const uint64_t size);
-#define HashTable_create(T, size) internal_HashTable_create(sizeof(T), size)
+void HashTable_insert(HashTable* table, const short* alias, void* value);
+void HashTable_remove(HashTable* table, const short* alias);
+void HashTable_resize(HashTable* table, const unsigned long long size);
+#define HashTable_create(T, size) internal_HashTable_create(sizeof(T), (unsigned long long)size);
 
-bool internal_HashTable_find(const HashTable* table, const char* alias, void** outValue);
+bool internal_HashTable_find(const HashTable* table, const short* alias, void** outValue);
 #define HashTable_find(table, alias, outValue) (internal_HashTable_find(table, alias, (void**)outValue))
-#define HashTable_array_iterator(table) uint64_t i = 0; i < table->SlotsUsed; i++
+#define HashTable_array_iterator(table) (unsigned long long i = 0; i < table->SlotsUsed; i++)
 #define HashTable_array_at(T, table, i) ((T*)table->Array[table->ActiveIndicies[i]].Value)
+#define HashTable_array_key_at(T, table, i) ((T*)table->Array[table->ActiveIndicies[i]].Key)
+
 
 #ifdef __cplusplus
 }
